5c224978a98a3a3af365ddac243b910b48b73b1d
restimguay <resti.guay@gmail.com>
2018-12-02 01:59:53 +0800

    update init method

--- src/database/BaseDatabase.php
+++ src/database/BaseDatabase.php
@@ -10,9 +10,9 @@ class BaseDatabase extends BaseObject
     public $_dsn;
     public $_driver;
     private static $_connection;
-    public function connect(){
+    public function connect($user,$pass,$options){
         if(self::$_connection==null){
-            self::$_connection = new $this->_driver($this->_dsn);
+            self::$_connection = new $this->_driver($this->_dsn,$user,$password,$options);
         }
     }
 }
