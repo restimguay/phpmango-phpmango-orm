78bb88723f938d9e3d802ad197adafd0479d0e06
restimguay <resti.guay@gmail.com>
2018-12-05 05:40:00 +0800

    commit for next iteration

--- src/model/BaseModel.php
+++ src/model/BaseModel.php
@@ -25,12 +25,27 @@ class BaseModel extends Connection
     /**
      *@var string
      */
-    private $_sql;
+    private $_sql = '';
+
+    /**
+     *@var string
+     */
+    private $_filter = '';
+
+    /**
+     *@var integer
+     */
+    private $_limit = '';
+
+    /**
+     *@var integer
+     */
+    private $_offset = '';
 
     /**
      *@var array
      */
-    private $_parameters;
+    private $_parameters = [];
 
     /**
      * @param string $tableName
@@ -58,33 +73,89 @@ class BaseModel extends Connection
     /**
      * @return boolean
      */
-    public function execute(){
-        $filter = '';
-        foreach($this->_parameters as $field=>$value){
-            if($filter==''){
-                $filter.=$field.'=:v_'.$field;
-            }else{
-                $filter.=' and :f_'.$field.'=:v_'.$field;
-            }
-        }
-        if($filter!==''){
-            $filter = ' WHERE '.$filter;
-        }
-        $this->_sql.=$filter;
-        $conn = $this->db();
-        $this->_stmt = $conn->prepare($this->_sql);
+    public function executeQuery(){
+        $this->buildFilters();
+        $this->_sql.= $this->getFilter();
+        $this->_sql.= $this->getLimitBy();
+        $this->_sql.= $this->getOffsetBy();
+        $this->_stmt = $this->db()->prepare($this->_sql);
+        $this->bindFilterValues();
+        $this->_stmt->execute();
+        return $this;
+    }
 
+    /**
+     * @return string ex. LIMIT <limiNumber>
+     */
+    private function getFilter(){        
+        if($this->filter!==''){
+            $this->filter = ' WHERE '.$this->filter;
+        }
+        return $this->_filter;
+    }
+    /**
+     * @return BaseModel
+     * @param array $parameters
+     */
+    public function where($parameters){
+        $this->buildFilters($parameters);
+        return $this;
+    }
+    /**
+     * @return BaseModel
+     */
+    public function limitBy(int $limit){
+        $this->_limit = ' LIMIT '.$limit;
+        return $this;
+    }
+    /**
+     * @return string ex. LIMIT <limiNumber>
+     */
+    private function getLimitBy(){
+        return $this->_limit;
+    }
+    /**
+     * @return string ex. OFFSET <limiNumber>
+     */
+    private function getOffsetBy(){
+        return $this->_offset;
+    }
+    /**
+     * @return BaseModel
+     */
+    public function offsetBy($offset){
+        $this->_offset = ' OFFSET '.$offset;
+        return $this;
+    }
+    /**
+     * @return BaseModel
+     */
+    private function bindFilterValues(){
         foreach($this->_parameters as $field=>&$value){
             $this->_stmt->bindValue('v_'.$field,$value);
         }
-        $this->_stmt->execute();
+        return $this;
+    }
+
+    /**
+     * @return BaseModel
+     */
+    private function buildFilters(){
+        $this->filter = '';
+        foreach($this->_parameters as $field=>$value){
+            if($this->filter==''){
+                $this->filter.=$field.'=:v_'.$field;
+            }else{
+                $this->filter.=' and :f_'.$field.'=:v_'.$field;
+            }
+        }
         return $this;
     }
     /**
      * @return array
      */
     public function fetchAssoc(){
-        $this->execute();
+        $this->executeQuery();
         return $this->_stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
     
@@ -92,7 +163,7 @@ class BaseModel extends Connection
      * @return array
      */
     public function fetchObject(){
-        $this->execute();
+        $this->executeQuery();
         return $this->_stmt->fetchAll(\PDO::FETCH_OBJ);
     }
 
--- test/db_test.php
+++ test/db_test.php
@@ -1,25 +1,24 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-$config = array_merge(
-    require __DIR__.'/../config/database.php'
-);
-define('DB_ENVIRONMENT','dev');
-use MGO\database\Connection;
-use MGO\database\DBConfig;
-use MGO\model\BaseModel;
+    require __DIR__.'/../vendor/autoload.php';
+    $config = array_merge(
+        require __DIR__.'/../config/database.php'
+    );
+    define('DB_ENVIRONMENT','dev');
+    use MGO\database\Connection;
+    use MGO\database\DBConfig;
+    use MGO\model\BaseModel;
 
 
-$dbConfig = new DBConfig();
-$dbConfig->setConfig($config['db'],DB_ENVIRONMENT);
-$conn = new Connection($dbConfig);
+    $dbConfig = new DBConfig();
+    $dbConfig->setConfig($config['db'],DB_ENVIRONMENT);
+    $conn = new Connection($dbConfig);
 
-$conn->setConfig($dbConfig);
-$conn->start();
+    $conn->setConfig($dbConfig);
+    $conn->start();
 
-$db = new BaseModel();
-$result = $db->findIn('member')->by(['id'=>1])->fetchAssoc();
-var_dump($result);
-$result = $db->findIn('member')->by(['id'=>1])->fetchObject();
-var_dump($result);
-
+    $db = new BaseModel();
+    $result = $db->findIn('member')->where(['id'=>1])->limitBy(1)->fetchAssoc();
+    var_dump($result);
+    //$result = $db->findIn('member')->where(['id'=>1])->fetchObject();
+    //var_dump($result);
